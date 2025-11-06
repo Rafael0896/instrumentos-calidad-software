fb77e90bf57df9ef8ffc3b9b2c366e1b41ba10de
Rafael0896 <realvarez54@misena.edu.co>
2025-11-06 08:31:53 -0500

    new file: src/App.jsx, modified: index.html

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+// src/main.jsx
+import './style.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+const rootElement = document.getElementById('root')
+if (!rootElement) throw new Error('No se encontró el elemento #root')
+
+createRoot(rootElement).render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+)
